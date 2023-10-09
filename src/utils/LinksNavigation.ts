fee9750322136667140ad0e555c8951e4e7064fe
André Carlos <andresinho200498@gmail.com>
2023-10-08 22:48:25 -0300

    new contents

--- src/utils/LinksNavigation.ts
+++ src/utils/LinksNavigation.ts
@@ -1,7 +1,7 @@
 export const LinksNavigation:ILinkNav[] = [
 	{ name: "Articles", href: "/articles" },
 	{ name: "Colaboratory", href: "/colaboratory" },
-	{ name: "Contact", href: "/contact" },
+	// { name: "Contact", href: "/contact" },
 	{ name: "About", href: "/about" },
   ];
   
